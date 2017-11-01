b73fffbe380e50e23f1dbda9d449f72e80688ba7
Nick-tsoukas <nick.tsoukas101@gmail.com>
2017-10-31 22:59:29 -0400

    Clean Webpack Config With Babel env preset

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ let config = {
   entry: './index.js',
   output: {
     filename: 'output.js'
-  }
+  },
+  module: {
+  rules: [
+    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  ]
+}
 }
 
 module.exports = config;
